c31e5b48448fed86b720d4a6829b6cb0aa71d836
danendra <dzaki.danen19@gmail.com>
2026-09-03 20:17:35 +0700

    edit nama file

--- pertemuan2.py
+++ pertemuan2.py
@@ -1,26 +1,25 @@
-# Soal Nomor 1
-nama = "Danendra Dzaki Andita"
-umur = 18
-bb = 65.5
+print('hello gesss')
 
-print("Nama : ", nama)
-print("Umur : ", umur, "tahun")
-print("Berat Badan : ", bb, "kg")
+data_integer = 1
+print("data : ", data_integer)
+print("- bertipe ", type(data_integer))
 
-# Soal Nomor 2
-angka_string = "123"
-angka_float = 45.67
-angka_integer = 89
+data_float = 1.5
+print("data : ", data_float)
+print("- bertipe ", type(data_float))
 
-print(int(angka_string))
-print(int(angka_float))
-print(float(angka_integer))
-print(str(angka_integer))
+data_string = "ucup"
+print("data : ", data_string)
+print("- bertipe ", type(data_string))
 
-usia = (int(input("Masukkan usiamu : ")))
-tb = (float(input("Masukkan tinggi badanmu : ")))
-nama = (str(input("Masukkan namamu : ")))
+data_bool = True
+print("data : ", data_bool)
+print("- bertipe ", type(data_bool))
 
-print("Nama : ", nama)
-print("Usia : ", usia, "tahun") 
-print("Tinggi Badan : ", tb, "cm")
+data_int = 9
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int) 
+print("data = ", data_float, ",type = ", type(data_float))
+print("data = ", data_str, ",type = ", type(data_str))
+print("data = ", data_bool, ",type = ", type(data_bool))
