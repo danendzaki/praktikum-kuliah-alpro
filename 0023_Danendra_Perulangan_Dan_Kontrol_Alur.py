104646271783ce5404397a104fb187d9fecb6cfe
danendra <dzaki.danen19@gmail.com>
2026-09-25 00:50:34 +0700

    Update tugas 5

--- 0023_Danendra_Perulangan_Dan_Kontrol_Alur.py
+++ 0023_Danendra_Perulangan_Dan_Kontrol_Alur.py
@@ -12,4 +12,12 @@ while angka < 50:
     angka += 1
     if angka % 2 != 0:
         continue
-    print(angka)
+    print(angka) 
+    
+# Bilangan prima
+for i in range(2, 101):
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        print(i)
